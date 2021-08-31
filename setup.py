2ab61bf791bbad17e7625577516f6ffbece313dc
Riju Mukherjee <64312216+rijulizer@users.noreply.github.com>
2021-08-31 16:41:21 +0530

    setup file changed

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
     python_requires='>=3.6',
 )
